6337f77cbc7add96c9524591b06baf007344dc0b
Sterling Walker <lazarus_star@hotmail.com>
2013-11-24 21:42:09 -0500

    Added launch functionality to cases.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 require './lib/student.rb'
 require './lib/scraper.rb'
 require 'awesome_print'
+require 'launchy'
 
 student_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
 
@@ -14,12 +15,29 @@ index_counter = 0
 student_directory = []
 
 name_array.each do |name|
-  student = Student.new(name, blog_array[index_counter], twitter_array[index_counter])
-  student_directory << student
+  student_directory << Student.new(name, twitter_array[index_counter], blog_array[index_counter])
   index_counter += 1
 end
 
-puts student_directory
+student_directory.sort! {|student1, student2| student1.name <=> student2.name}
+
+def directory(student_directory)
+    puts "Please enter the number of the requested student."
+    student_directory.each_with_index do |student, index|
+        puts "#{index+1}. #{student.name}"
+    end
+    request_index = gets.chomp.to_i
+    request_index -1
+end
+
+def launch (media, student_directory)
+    student_index = directory(student_directory)
+   if student_directory[student_index].media == "none"
+    puts "Sorry that student has not shared a #{media} account."
+   else
+    Launchy.open("#{student_directory[student_index].media}")
+   end
+    end
 
 
 puts "Do you want to look up the Twitter of a specific student? Type: 'T'"
@@ -32,13 +50,17 @@ response = gets.chomp
 case
 
 when response.downcase == "t"
-    ##
+  launch("Twitter", student_directory)
 when response.downcase == "b"
-    ##
+    launch("blog", student_directory)
 when response.downcase == "r"
-    ##
+    random_twitter = student_directory.sample.twitter
+    random_twitter = student_directory.sample.twitter if random_twitter == "none"
+     Launchy.open("#{student_directory.sample.twitter}")
 when response.downcase == "l"
-    ##
+    random_blog = student_directory.sample.blog
+    random_blog = student_directory.sample.blog if random_blog == "none"
+    Launchy.open("#{student_directory.sample.blog}")
 when response.downcase == "exit"
     puts "Goodbye"
 else 
